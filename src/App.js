8e43288daa6308f751f7532c58508973fbf122ea
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-07-14 15:56:29 -0700

    refatcored App.js with redux hooks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React from "react";
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import Header from "./components/Header";
 import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
 
 const App = (props) => {
-  
+  const { car, additionalFeatures, additionalPrice } = useSelector(
+    (state) => state
+  );
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={props.car} />
-        <AddedFeatures car={props.car} />
+        <Header car={car} />
+        <AddedFeatures car={car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
-        <Total car={props.car} additionalPrice={props.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={additionalFeatures} />
+        <Total car={car} additionalPrice={additionalPrice} />
       </div>
     </div>
   );
 };
 
-const mapStateToProps = state => {
- return {
-   car: state.car,
-   additionalFeatures: state.additionalFeatures,
-   additionalPrice: state.additionalPrice
- }
-}
+// const mapStateToProps = (state) => {
+//   return {
+//     car: state.car,
+//     additionalFeatures: state.additionalFeatures,
+//     additionalPrice: state.additionalPrice,
+//   };
+// };
 
-export default connect(mapStateToProps, {})(App);
+export default App;
+
+// export default connect(mapStateToProps, {})(App);
